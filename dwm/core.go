d1631ca98419312942e39b5394ef7db0a78ae105
Alex Kesling <alex.kesling@gmail.com>
2015-12-30 10:20:21 -0500

    Restructured repo to be workspace compliant.

--- dwm/core.go
+++ dwm/core.go
@@ -12,3 +12,29 @@ package dwm
 int main_impl(int argc, char* argv[]);
 */
 import "C"
+
+import (
+	"os"
+	"unsafe"
+)
+
+func MainImpl(argc int, argv **C.char) C.int {
+	return C.main_impl(C.int(argc), argv)
+}
+
+func GoToCArgumentList(go_args []string) **C.char {
+	c_args := (**C.char)(C.malloc(C.size_t(len(go_args)) * sizeOfChar()))
+	for i := range os.Args {
+		*c_args = C.CString(os.Args[i])
+	}
+	return c_args
+}
+
+func FreeCArgs(c_args **C.char) {
+	C.free(unsafe.Pointer(c_args))
+}
+
+func sizeOfChar() C.size_t {
+	var b *C.char
+	return C.size_t(unsafe.Sizeof(b))
+}
